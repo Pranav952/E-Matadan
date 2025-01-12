ef23992314f6bb902a1fd7724719506c1a5f3d14
Subash Singh Dhami <subashdhamee@gmail.com>
2025-01-12 17:01:13 +0545

    user view added

--- backend/django/user/urls.py
+++ backend/django/user/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views import FeedbackView
+from .views import ProtectedView
 
 urlpatterns = [
     path('api/feedback/', FeedbackView.as_view(), name='feedback'),
+      path("protected/", ProtectedView.as_view(), name="protected"),
 ]
 
--- backend/django/user/views.py
+++ backend/django/user/views.py
@@ -1,6 +1,10 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+
 from .models import Feedback
 from .serializer import FeedbackSerializer
 
@@ -11,6 +15,13 @@ class FeedbackView(APIView):
             serializer.save()
             return Response({"message": "Feedback submitted successfully"}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+      
+class ProtectedView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        return Response({"message": f"Hello, {request.user}!"})  # Access user from the token
+
 
     
     
