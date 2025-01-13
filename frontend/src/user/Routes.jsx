c345285f0a4095c154f6469897cca0e12b25f48a
Ankit-47 <omkathet218@gmail.com>
2025-01-13 13:19:00 +0545

    protected routes added for all

--- frontend/src/user/Routes.jsx
+++ frontend/src/user/Routes.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route,Routes } from "react-router-dom";
 import Dashboard from "./views/DashBoard";
 import ApplyForVoting from "./views/RegisterAsVoter";
 import Vote from "./views/Votes";
@@ -10,27 +10,97 @@ import Notification from "./views/Notifications";
 import Result from "./views/Results";
 import Login from "./views/Login";
 import Register from "./views/Register";
+import ProtectedRoute from "../utils/ProtectedRoute";
 import { Navigate } from "react-router-dom";
-
-function UserRoutes() {
-  return (
-    <Routes>
+import RegisterAsCandidate from "./views/RegisterAsCandidate";
+function UserRoutes()
+{
+    return(
+        <Routes>
       {/* Default route for /user */}
-      <Route path="/" element={<Navigate to="dashboard" replace />} />
-      <Route path="dashboard" element={<Dashboard />} />
-      <Route path="voting-apply" element={<ApplyForVoting />} />
-      <Route path="vote" element={<Vote />} />
-      <Route path="results" element={<Result />} />
-      <Route path="feedback" element={<FeedBack />} />
-      <Route path="support" element={<Support />} />
-      <Route path="settings" element={<Setting />} />
-      <Route path="notifications" element={<Notification />} />
-      
-      {/* Uncomment the login route if necessary */}
-      <Route path='/login' element={<Login/>}/>
-      <Route path="register" element={<Register />} />
-    </Routes>
-  );
-}
+      <Route
+        path="/"
+        element={
+          <ProtectedRoute requiredRole="user">
+            <Navigate to="dashboard" replace />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="dashboard"
+        element={
+          <ProtectedRoute requiredRole="user">
+            <Dashboard />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="voter-register"
+        element={
+          <ProtectedRoute requiredRole="user">
+            <ApplyForVoting />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="candidate-register"
+        element={
+          <ProtectedRoute requiredRole="user">
+            <RegisterAsCandidate/>
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="vote"
+        element={
+          <ProtectedRoute requiredRole="user">
+            <Vote />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="results"
+        element={
+          <ProtectedRoute requiredRole="user">
+            <Result />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="feedback"
+        element={
+          <ProtectedRoute requiredRole="user">
+            <FeedBack />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="support"
+        element={
+          <ProtectedRoute requiredRole="user">
+            <Support />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="settings"
+        element={
+          <ProtectedRoute requiredRole="user">
+            <Setting />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="notifications"
+        element={
+          <ProtectedRoute requiredRole="user">
+            <Notification />
+          </ProtectedRoute>
+        }
+      />
 
+    <Route path='login' element={<Login/>}/>
+        </Routes>
+    )
+}
 export default UserRoutes;
